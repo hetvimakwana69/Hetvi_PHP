364046425c536b1b692e2110f3476a51d88d9e03
hetvimakwana69 <hetvimakwana1533@gmail.com>
2022-07-23 12:41:54 +0530

    changed

---
+++ Extra page/manage_emp.php
@@ -0,0 +1,74 @@
+<?php
+include_once('header.php');
+?>
+ 
+ 	<!--banner-->	
+		     <div class="banner">
+		    	<h2>
+				<a href="dashboard">Home</a>
+				<i class="fa fa-angle-right"></i>
+				<span>Manage Employee</span>
+				</h2>
+		    </div>
+		<!--//banner-->
+ 	 <!--faq-->
+ 	<div class="blank">
+	
+
+			<div class="blank-page">
+				
+				<div class="container mt-3">
+				  <h2>Manage Employee</h2>
+
+				  <table class="table">
+					<thead>
+					  <tr>
+					     <th>Emp_id</th>  
+						<th>Name</th>
+						<th>Username</th>
+						<th>Email_id</th>
+						<th>Password</th>
+						<th>Address</th>
+						<th>Contact_no</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+					    <td>1</td>
+						<td>Hetvi</td>
+						<td>Hkm</td>
+						<td>Hetvi28@gmail.com</td>
+						<td>2356</td>
+						<td>Maninagar</td>
+						<td>32659874</td>
+						<td><a href="#" class="btn btn-primary">Edit</a></td>
+						<td><a href="#" class="btn btn-danger">Delete</a></td>
+					  </tr>
+					  
+					  <tr>
+					    <td>2</td>
+						<td>Khushi</td>
+						<td>KVyas</td>
+						<td>Khushi98@gmail.com</td>
+						<td>5698</td>
+						<td>Ghodasar</td>
+						<td>326523658</td>
+						<td><a href="#" class="btn btn-primary">Edit</a></td>
+						<td><a href="#" class="btn btn-danger">Delete</a></td>
+					  </tr>
+					  
+					  
+					  
+					</tbody>
+				  </table>
+				</div>
+				
+	        </div>
+	</div>
+	
+	<!--//faq-->
+	<?php
+include_once('footer.php');
+?>
---
+++ Extra page/manage_feedback.php
@@ -0,0 +1,60 @@
+<?php
+include_once('header.php');
+?>
+ 
+ 	<!--banner-->	
+		     <div class="banner">
+		    	<h2>
+				<a href="dashboard">Home</a>
+				<i class="fa fa-angle-right"></i>
+				<span>Manage Feedback</span>
+				</h2>
+		    </div>
+		<!--//banner-->
+ 	 <!--faq-->
+ 	<div class="blank">
+	
+
+			<div class="blank-page">
+				
+				<div class="container mt-3">
+				  <h2>Manage Feedback</h2>
+
+				  <table class="table">
+					<thead>
+					  <tr>
+						<th>Feedback_id</th>
+						<th>cust_id</th>
+						<th>comment</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+						<td>101</td>
+						<td>1</td>
+						<td>good service</td>
+						<td><a href="#" class="btn btn-primary">Edit</a></td>
+						<td><a href="#" class="btn btn-danger">Delete</a></td>
+					  </tr>
+					  
+					  <tr>
+						<td>102</td>
+						<td>2</td>
+						<td>good service provide</td>
+						<td><a href="#" class="btn btn-primary">Edit</a></td>
+						<td><a href="#" class="btn btn-danger">Delete</a></td>
+					  </tr>
+					  
+					</tbody>
+				  </table>
+				</div>
+				
+	        </div>
+	</div>
+	
+	<!--//faq-->
+	<?php
+include_once('footer.php');
+?>
